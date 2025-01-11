36a20b7e7db4573406502d92e90a3d7334d5caa0
CAMILLEG <cgyrd@outlook.fr>
2025-01-11 16:08:18 +0100

    pushing projet on Github

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <section className="page-entiere">
+        <section className="titre-jeu">
+          <h2>Même dans le flou, je vois bien que t'es pas net!</h2>
+        </section>
+        <section className="explication">
+        
+            <p>
+           
+              Bienvenue au bal ! <br></br> Il n'y a pas vraiment de règles ! Tu
+              vas devoir retrouver l'intru qui se cache parmis les invités.
+              Attention à ne pas sortir des invités VIP du jeu sinon, les vigils
+              te sortiront de la pièce, et tu n'auras plus qu'à rentrer chez
+              toi...
+            </p>
+            <img
+              src="/personnages/man-rule.png"
+              alt="Man"
+              className="homme-explication"
+            />
+     
+        </section>
+        <section className="Play">
+          <button className="press-play">PLAY </button>
+        </section>
+      </section>
+    </>
+  );
+}
+
+export default App;
